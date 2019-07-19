1888a945733b003058d60fdc2355153ccb832f5d
icestains <ourlichi@gmail.com>
2019-07-19 16:30:27 +0800

    'setupOpenLayers'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <OpenLayers/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import OpenLayers from './components/openlayers'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            OpenLayers
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body, html {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    #app {
+        height: 100%;
+    }
 </style>
---
+++ src/components/openlayers.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="rootMap" class="map"></div>
+</template>
+
+<script>
+
+    import 'ol/ol.css'
+    import {Map, View} from 'ol'
+    import olTitle from 'ol/layer/Tile'
+    import olOSM from 'ol/source/OSM'
+    import {get as getProjection} from 'ol/proj';
+
+
+    export default {
+        name: "rootMap",
+        data() {
+            return {
+                map: null,
+                view: null
+            }
+        },
+        mounted() {
+            this.init();
+        },
+        methods: {
+            init() {
+                var projection = getProjection('EPSG:4326');
+
+                this.map = new Map({
+                    target: 'rootMap',
+                    layers: [
+                        new olTitle({
+                            source: new olOSM()
+                        })
+                    ],
+                    view: new View({
+                        center: [120.58, 31.30],
+                        projection: projection,
+                        zoom: 12
+                    })
+                })
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .map {
+        height: 100%;
+        width: 100%;
+    }
+
+</style>
